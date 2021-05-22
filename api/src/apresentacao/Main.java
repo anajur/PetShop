a7ae8b178e5f558c9d0c2c087e12d6365825cf07
Ana Julia da Rosa <ana.rosa@cwi.com.br>
2021-05-22 19:31:32 -0300

    adicionando controllers

--- api/src/apresentacao/Main.java
+++ api/src/apresentacao/Main.java
@@ -1,75 +1,26 @@
 package apresentacao;
 
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
 
-import entities.Animal;
-import entities.Atendimento;
-import entities.Endereco;
-import entities.Pessoa;
-import entities.PetShop;
-import enums.Especie;
-import enums.Servicos;
-import enums.Sexo;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
+import controllers.PetShopController;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		Scanner teclado = new Scanner(System.in);
-				
-		Pessoa cliente = new Pessoa(
-				"Lorelai Campos", 
-				"051998986865", 
-				"lorelai@gmail.com",
-				new Endereco(
-						"Porto Alegre", 
-						"Zona Sul", 
-						302, 
-						57073038, 
-						"casa"), 
-				"01355046009",
-				new ArrayList<Animal>()
-				);
-		
-		Animal cachorro = new Animal(
-				"Labrador", 
-				"Caramelo", 
-				LocalDate.parse("2000-02-02"), 
-				cliente, 
-				"nenhuma", 
-				Sexo.M,
-				Especie.CACHORRO
-				);
 
-		List<Servicos> servicos = new LinkedList<>();
-		servicos.add(Servicos.TOSA);
-		servicos.add(Servicos.BANHO);
-		
-		Atendimento atendimento = new Atendimento(cachorro, LocalDate.now(), 30.20, Servicos.BANHO);
-		
-		List<Atendimento> atendimentos = new ArrayList<>();
-		
-		PetShop petShop = new PetShop();
-		
+		Scanner teclado = new Scanner(System.in);
+
+		PetShopController petShop = new PetShopController();
+
 		int escolha;
-		String escolhaServico;
-		
-		System.out.println("Dog Caramelo");
-		
+
 		do {
-			
+
 			petShop.exibirMenu();
 			escolha = teclado.nextInt();
 			petShop.escolherOpcaoMenu(escolha);
-			
+
 		} while (escolha != 4);
-		
-		System.out.println("Obrigado e volte sempre.");
-		
+
+		teclado.close();
 	}
 }
--- api/src/entities/Atendimento.java
+++ api/src/entities/Atendimento.java
@@ -3,59 +3,56 @@ package entities;
 import enums.Servicos;
 
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
 
 public class Atendimento {
-	
-    private Animal animal;
-    private Servicos servicoSelecionado;
-    private LocalDate diaAtendimento;
-    private Double preco;
-    private Boolean encerrado;
 
-    public Atendimento(Animal animal, LocalDate diaAtendimento, Double preco, Servicos servicoSelecionado) {
-        this.animal = animal;
-        this.diaAtendimento = diaAtendimento;
-        this.preco = preco;
-        this.servicoSelecionado = servicoSelecionado;
-        this.encerrado = false;
-    }
-    
-    public Atendimento() {}
+	private Animal animal;
+	private Servicos servicoSelecionado;
+	private LocalDate diaAtendimento;
+	private Double preco;
+	private Boolean encerrado = false;
 
-    public boolean isEncerrado() {
-        return encerrado;
-    }
+	public Atendimento(Animal animal, LocalDate diaAtendimento, Double preco, Servicos servicoSelecionado) {
+		this.animal = animal;
+		this.diaAtendimento = diaAtendimento;
+		this.preco = preco;
+		this.servicoSelecionado = servicoSelecionado;
+	}
 
-    public void setEncerrado(boolean encerrado) {
-        this.encerrado = encerrado;
-    }
+	public Atendimento() {
+	}
 
-    public Animal getAnimal() {
-        return animal;
-    }
+	public boolean isEncerrado() {
+		return encerrado;
+	}
 
-    public void setAnimal(Animal animal) {
-        this.animal = animal;
-    }
+	public void setEncerrado(boolean encerrado) {
+		this.encerrado = encerrado;
+	}
 
-    public LocalDate getDiaAtendimento() {
-        return diaAtendimento;
-    }
+	public Animal getAnimal() {
+		return animal;
+	}
 
-    public void setDiaAtendimento(LocalDate diaAtendimento) {
-        this.diaAtendimento = diaAtendimento;
-    }
+	public void setAnimal(Animal animal) {
+		this.animal = animal;
+	}
 
-    public Double getPreco() {
-        return preco;
-    }
+	public LocalDate getDiaAtendimento() {
+		return diaAtendimento;
+	}
 
-    public void setPreco(Double preco) {
-        this.preco = preco;
-    }
+	public void setDiaAtendimento(LocalDate diaAtendimento) {
+		this.diaAtendimento = diaAtendimento;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
 
 	public Servicos getServicoSelecionado() {
 		return servicoSelecionado;
@@ -64,4 +61,11 @@ public class Atendimento {
 	public void setServicoSelecionado(Servicos servicoSelecionado) {
 		this.servicoSelecionado = servicoSelecionado;
 	}
+
+	@Override
+	public String toString() {
+		return "Atendimento [animal=" + animal + ", servicoSelecionado=" + servicoSelecionado + ", diaAtendimento="
+				+ diaAtendimento + ", preco=" + preco + ", encerrado=" + encerrado + "]";
+	}
+
 }
